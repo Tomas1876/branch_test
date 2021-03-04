1f154a2425ad193100fe9f4879df61a156131ee1
AFTERNOON\Kahee <be_nothing@naver.com>
2021-03-05 00:45:49 +0900

    tired

--- src/branch_test/Main.java
+++ src/branch_test/Main.java
@@ -11,6 +11,7 @@ public class Main {
            System.out.println("hyunjin");
            System.out.println("stay1 YOUNGHOON");
            System.out.println("I'm trying!!!!!");
+           System.out.println("tired....");
         
         System.out.println("This is stay Fork and Fetch Practice now");
                 System.out.println("fetch and merger retry");
